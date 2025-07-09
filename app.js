da11d7d4c37d60e4f734619dc635ca5bc34b62a1
Thalia Mijas <tlmijas@outlook.com>
2025-07-08 22:20:20 -0500

    Proteger logout con csrf

--- app.js
+++ app.js
@@ -30,14 +30,7 @@ app.get("/csrf-token", csrfProtection, (req, res) => {
 
 app.get("/", listUsers, (req, res) => {});
 
-app.post(
-  "/",
-  verifyToken,
-  csrfProtection,
-  validateUser,
-  createUser,
-  (req, res) => {}
-);
+app.post("/", csrfProtection, validateUser, createUser, (req, res) => {});
 
 app.put(
   "/:id",
@@ -56,7 +49,7 @@ app.post("/login", loginLimiter, csrfProtection, generateToken, (req, res) => {
   res.status(200).json({ message: "Login exitoso" });
 });
 
-app.post("/logout", (req, res) => {
+app.post("/logout", csrfProtection, (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: true,
